aaa5260ef551fa90d7bc487e72f6bb62918a2bdb
ekaone <ekaone3033@gmail.com>
2021-02-11 08:48:55 +0700

    update store

--- src/stores/order.js
+++ src/stores/order.js
@@ -1,3 +1,20 @@
 import { proxy } from "valtio";
 
-export const state = proxy({ count: 10 });
+export const state = proxy({
+  count: 10,
+  users: [
+    { id: 1, username: "root@mail.com", password: "123" },
+    { id: 2, username: "admin@mail.com", password: "123" },
+    { id: 3, username: "user@mail.com", password: "123" },
+  ],
+  foods: [
+    { id: 1, name: "Nasi Putih", price: "5000" },
+    { id: 2, name: "Tempe Goreng", price: "2000" },
+    { id: 3, name: "Sayur Terong", price: "3000" },
+  ],
+  beverages: [
+    { id: 1, name: "Es Teh Manis", price: "2500" },
+    { id: 2, name: "Air Mineral", price: "2000" },
+    { id: 3, name: "Es Campur", price: "7500" },
+  ],
+});
