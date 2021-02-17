25d16889403319af1a7bc40fa06a4afe3fdfb683
ekaone <ekaone3033@gmail.com>
2021-02-17 17:37:00 +0700

    add table

--- src/containers/TheFooter.js
+++ src/containers/TheFooter.js
@@ -1,19 +1,31 @@
-import React from 'react'
-import { CFooter } from '@coreui/react'
+import React from "react";
+import { CFooter } from "@coreui/react";
 
 const TheFooter = () => {
   return (
     <CFooter fixed={false}>
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">CoreUI</a>
-        <span className="ml-1">&copy; 2020 creativeLabs.</span>
+        <a
+          href="https://ekaprasetia.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Eka
+        </a>
+        <span className="ml-1">&copy; 2021 Eka.</span>
       </div>
       <div className="mfs-auto">
         <span className="mr-1">Powered by</span>
-        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">CoreUI for React</a>
+        <a
+          href="https://ekaprasetia.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Eka
+        </a>
       </div>
     </CFooter>
-  )
-}
+  );
+};
 
-export default React.memo(TheFooter)
+export default React.memo(TheFooter);
--- src/views/order/order/index.js
+++ src/views/order/order/index.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { CCard, CCardBody, CCol, CRow } from "@coreui/react";
 import Users from "../../users/Users";
 
 function Order() {
   return (
-    <div>
-      <Users />
-    </div>
+    <>
+      <CRow>
+        <CCol xs="12" lg="6">
+          <Users header={"Users Active"} />
+        </CCol>
+
+        <CCol xs="12" lg="6">
+          <Users header={"Companies"} />
+        </CCol>
+      </CRow>
+    </>
   );
 }
 
